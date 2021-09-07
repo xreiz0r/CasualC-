0ba4dc306b2002e25fd166d6f5619529e506c443
xreiz <89862635+xreiz@users.noreply.github.com>
2021-09-07 02:10:58 +0000

    Add files via upload

---
+++ gitups/translateru.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+
+using namespace std;
+
+	int main() {
+	
+	cout << "W e l c o m e ...\nTranslate a number:\n RU[ I - X ]\n\n";
+		
+	int i;
+	cin >> i;
+	
+	
+		
+		
+	if(i > 0){
+	
+	switch(i)
+		{
+		case 10:
+		cout << "d e s s i t .\n\n";
+		break;
+		
+		case 9:
+		cout << "d e v i t s .\n\n";
+		break;
+		
+		case 8:
+		cout << "v o s e m .\n\n";
+		break;
+		
+		case 7:
+		cout << "s e e m .\n\n";
+		break;
+		
+		case 6:
+		cout << "s h e e s t .\n\n";
+		break;
+		
+		case 5:
+		cout << "p i e t t\n\n";
+		break;
+		
+		case 4:
+		cout << "c h i t i r r e .\n\n";
+		break;
+		
+		case 3:
+		cout << "t h g i .\n\n";
+		break;
+		
+		case 2:
+		cout << "d h v a .\n\n";
+		break;
+		
+		case 1:
+		cout << "a j i n. \n\n";
+		break;
+		default :
+		cout << "I n v a l i d  e n t r y . ..\n";
+		 }
+		   	}
+	else {
+	cout << "I n v a l i d  e n t r y  ...\n";
+	}		
+	return 0;
+		                                                            }
+

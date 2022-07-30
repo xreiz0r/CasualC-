1acd7e02368531bb6901537b915389fe5180f17d
R3IS <101276269+xreiz0r@users.noreply.github.com>
2022-07-30 09:43:35 +0100

    Update WeekdaysRu.cpp

--- Foreign-Week/WeekdaysRu.cpp
+++ Foreign-Week/WeekdaysRu.cpp
@@ -28,13 +28,13 @@ cin >> i;
 if(i>0){
 switch(i){
 
-	case 1:cout << "\nPonil dinik\n";break;
-	case 2:cout << "\nTornik\n";break;
-	case 3:cout << "\nShida\n";break;
-	case 4:cout << "\nShitver\n";break;
-	case 5:cout << "\nPyanissa\n";break;
-	case 6:cout << "\nSuvota\n";break;
-	case 7:cout << "\nVoscreseen\n";break;
+	case 1:cout << "\n \n";break;
+	case 2:cout << "\n \n";break;
+	case 3:cout << "\n \n";break;
+	case 4:cout << "\n \n";break;
+	case 5:cout << "\n \n";break;
+	case 6:cout << "\n \n";break;
+	case 7:cout << "\n \n";break;
 	default: cout << "\nInvalid entry.. .\n";
 }
 }
